2ccf917752b89ed441028b5ea9adfa372dddeddc
Violet Siegius <fvcproductions@icloud.com>
2015-08-22 11:01:21 -0400

    Project Page Done
    Signed-off-by: Violet Siegius <fvcproductions@icloud.com>

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,9 +3,6 @@ var crypto = require('crypto');
 var mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
-    name: {
-        type: String
-    },
     email: {
         type: String,
         required: true,
