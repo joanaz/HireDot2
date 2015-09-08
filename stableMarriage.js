70c9c8a6239161e0f9efb0f8701a13a153c7fdee
Violet Siegius <fvcproductions@icloud.com>
2015-09-08 15:24:39 -0400

    stableMarriage was icky

--- stableMarriage.js
+++ stableMarriage.js
@@ -10,28 +10,19 @@ function Person(name) {
         for (i = 0; i < this.candidates.length; i++)
             if (this.candidates[i] === p) return i;
         return this.candidates.length + 1;
-<<<<<<< HEAD
-    }
 
-    this.prefers = function(p) {
-        return this.rank(p) < this.rank(this.fiance);
-    }
-=======
     };
 
     this.prefers = function(p) {
         return this.rank(p) < this.rank(this.fiance);
     };
->>>>>>> master
+
 
     this.nextCandidate = function() {
         if (candidateIndex >= this.candidates.length) return null;
         return this.candidates[candidateIndex++];
-<<<<<<< HEAD
-    }
-=======
+
     };
->>>>>>> master
 
     this.engageTo = function(p) {
         if (p.fiance) {
@@ -43,11 +34,10 @@ function Person(name) {
         if (this.fiance) this.fiance.fiance = null;
         this.fiance = p;
         // console.log(this.name, this.fiance.name)
-<<<<<<< HEAD
-    }
-=======
+
+
     };
->>>>>>> master
+
 
     this.swapWith = function(p) {
         console.log("%s & %s swap partners", this.name, p.name);
@@ -55,11 +45,7 @@ function Person(name) {
         var pFiance = p.fiance;
         this.engageTo(pFiance);
         p.engageTo(thisFiance);
-<<<<<<< HEAD
-    }
-=======
     };
->>>>>>> master
 }
 
 function isStable(guys, gals) {
@@ -76,81 +62,72 @@ function engageEveryone(guys) {
         done = true;
         for (var i = 0; i < guys.length; i++) {
             var guy = guys[i];
-<<<<<<< HEAD
-            console.log(guy.name)
-=======
             console.log(guy.name);
->>>>>>> master
 
             if (!guy.fiance) {
                 done = false;
 
                 var gal = guy.nextCandidate();
-<<<<<<< HEAD
                 console.log(gal.name)
                 if (!gal.fiance || gal.prefers(guy)) {
                     guy.engageTo(gal);
-                    console.log("engage", guy.name, gal.name)
-=======
-                console.log(gal.name);
-                if (!gal.fiance || gal.prefers(guy)) {
-                    guy.engageTo(gal);
                     console.log("engage", guy.name, gal.name);
->>>>>>> master
+                    if (!gal.fiance || gal.prefers(guy)) {
+                        guy.engageTo(gal);
+                        console.log("engage", guy.name, gal.name);
+                    }
                 }
+                // console.log(guy.name, guy.fiance.name)
             }
-            // console.log(guy.name, guy.fiance.name)
         }
-    } while (!done);
-}
-
-function doMarriage() {
-
-    var joanna = new Person("Joanna");
-    var frances = new Person("Frances");
-    var violet = new Person("Violet");
-
-    var google = new Person("Google");
-    var fb = new Person("Facebook");
-    // var twitter = new Person("Twitter");
-    // var linkedin=new Person('Linkedin');
-
-    joanna.candidates = [google, fb];
-    frances.candidates = [fb, google];
-    violet.candidates = [google, fb];
-
-    google.candidates = [frances, violet, joanna];
-    fb.candidates = [joanna, frances, violet];
-    // twitter.candidates = [frances, joanna, violet];
-    // linkedin.candidates=[joanna,frances,violet];
-
-    var people = [joanna, frances, violet];
-    var companies = [google, fb, ];
-
-<<<<<<< HEAD
-    var smallerSet
-    if (people.length <= companies.length)
-        smallerSet = people
-    else
-        smallerSet = companies
-
-    engageEveryone(smallerSet)
-=======
-    var smallerSet;
-    if (people.length <= companies.length)
-        smallerSet = people;
-    else
-        smallerSet = companies;
-
-    engageEveryone(smallerSet);
->>>>>>> master
-
-    for (var i = 0; i < smallerSet.length; i++) {
-        console.log("%s is engaged to %s", smallerSet[i].name, smallerSet[i].fiance.name);
+        while (!done);
     }
-    console.log("Stable = %s", isStable(people, companies) ? "Yes" : "No");
-    joanna.swapWith(frances);
-    console.log("Stable = %s", isStable(people, companies) ? "Yes" : "No");
-}
 
-doMarriage();
+    function doMarriage() {
+
+        var joanna = new Person("Joanna");
+        var annabelle = new Person("Annabelle");
+        var violet = new Person("Violet");
+
+        var google = new Person("Google");
+        var fb = new Person("Facebook");
+        // var twitter = new Person("Twitter");
+        // var linkedin=new Person('Linkedin');
+
+        joanna.candidates = [google, fb];
+        annabelle.candidates = [fb, google];
+        violet.candidates = [google, fb];
+
+        google.candidates = [annabelle, violet, joanna];
+        fb.candidates = [joanna, annabelle, violet];
+        // twitter.candidates = [annabelle, joanna, violet];
+        // linkedin.candidates=[joanna,annabelle,violet];
+
+        var people = [joanna, annabelle, violet];
+        var companies = [google, fb, ];
+
+
+        var smallerSet
+        if (people.length <= companies.length)
+            smallerSet = people
+        else
+            smallerSet = companies
+
+        engageEveryone(smallerSet)
+        var smallerSet;
+        if (people.length <= companies.length)
+            smallerSet = people;
+        else
+            smallerSet = companies;
+
+        engageEveryone(smallerSet);
+
+        for (var i = 0; i < smallerSet.length; i++) {
+            console.log("%s is engaged to %s", smallerSet[i].name, smallerSet[i].fiance.name);
+        }
+        console.log("Stable = %s", isStable(people, companies) ? "Yes" : "No");
+        joanna.swapWith(annabelle);
+        console.log("Stable = %s", isStable(people, companies) ? "Yes" : "No");
+    }
+
+    doMarriage();
